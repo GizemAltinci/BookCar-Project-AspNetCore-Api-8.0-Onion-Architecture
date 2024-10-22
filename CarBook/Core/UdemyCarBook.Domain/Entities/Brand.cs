2b872220f56690b0644e8229d9b16043460c321e
Gizem ALTINCI <gizemaltnc@outlook.com>
2024-10-22 13:07:43 +0300

    completed part2- Entities

---
+++ CarBook/Core/UdemyCarBook.Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public List<Car> Cars { get; set; }
+        public string Model { get; set; }
+        public string CoverImageUrl { get; set; }
+        public int Km { get; set; }
+        public string Transmisson { get; set; }
+        public byte Seat { get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+
+    }
+}
---
+++ CarBook/Core/UdemyCarBook.Domain/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Car
+    {
+        public int CarId { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public Lİst<CarPricing> CarPricings { get; set; }
+    }
+}
